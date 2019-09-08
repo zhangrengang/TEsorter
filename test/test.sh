d5a532684c532fb613a7460f359d353876a03ac7
zhangrengang <zhangrenang@ori-gene.cn>
2019-09-08 13:57:54 +0800

    update REXdb: viridiplantae_v3.0 + metazoa_v3 (#2)

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,15 @@
+# REXdb plants + metazoans
 python ../LTR_classifier.py LTRlibAnn rice6.9.5.liban
 python ../LTR_classifier.py Classifier rice6.9.5.liban.rexdb.gff3 > rice6.9.5.liban.rexdb.gff3.anno
 
+# GyDB
 python ../LTR_classifier.py LTRlibAnn rice6.9.5.liban gydb
 python ../LTR_classifier.py Classifier rice6.9.5.liban.gydb.gff3 gydb > rice6.9.5.liban.gydb.gff3.anno
 
+# REXdb plants
 python ../LTR_classifier.py LTRlibAnn rice6.9.5.liban rexdb-plant
 python ../LTR_classifier.py Classifier rice6.9.5.liban.rexdb-plant.gff3 rexdb-plant > rice6.9.5.liban.rexdb-plant.gff3.anno
+# REXdb metazoans
 python ../LTR_classifier.py LTRlibAnn rice6.9.5.liban rexdb-metazoa
 python ../LTR_classifier.py Classifier rice6.9.5.liban.rexdb-metazoa.gff3 rexdb-metazoa > rice6.9.5.liban.rexdb-metazoa.gff3.anno
 
